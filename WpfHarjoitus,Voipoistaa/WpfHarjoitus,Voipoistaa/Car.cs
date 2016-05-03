360fccdc5625606a37ce3857f5453a11b1b79f5a
Marko Viitanen <marko.viitanen@stud.pointcollege.fi>
2016-05-03 06:35:35 +0300

    XMLBooks

---
+++ WpfHarjoitus,Voipoistaa/WpfHarjoitus,Voipoistaa/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHarjoitus_Voipoistaa
+{
+    class Car
+    {
+        public string Color { get; set; }
+        public string Model  { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public void StartEngine()
+        {
+
+        }
+        public void StopEngine()
+        {
+
+        }
+    }
+}
